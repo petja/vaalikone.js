b08d46536683314eb830429f82a367eed4b33251
Petja <hello@petja.me>
2018-04-19 12:02:17 +0300

    Finishing login view
    App asks user credentials, queries API with them, and then stores JWT token received from the server to the IndexedDB

--- front/src/components/LoginView.jsx
+++ front/src/components/LoginView.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
+// Local
+import localForage from '../models/db'
+
+// Material UI
 import Typography from 'material-ui/Typography'
 import TextField from 'material-ui/TextField'
 import Button from 'material-ui/Button'
 import Paper from 'material-ui/Paper'
 import {withStyles} from 'material-ui/styles'
 
+// Third party
 import SwipeableViews from 'react-swipeable-views'
+
+const PAGE_EMAIL = 0
+const PAGE_PASSWORD = 1
 
 const styles = theme => ({
     root            : {
@@ -23,63 +31,122 @@ const styles = theme => ({
     },
 })
 
-const NextButton = ({classes}) => (
-    <Button
-        variant='raised'
-        color='secondary'
-        children='Jatka'
-        type='submit'
-        className={classes.button}
-    />
-)
+class LoginView extends React.Component {
+    state = {
+        page                : PAGE_EMAIL,
+    }
 
-const EmailPage = ({classes}) => (
-    <div>
-        <div className={classes.center}>
-            <TextField
-                label='Sähköpostiosoite'
-                type='email'
-                fullWidth
+    render() {
+
+        const {classes} = this.props
+
+        const nextButton = (
+            <Button
+                variant='raised'
+                color='secondary'
+                children='Jatka'
+                type='submit'
+                className={classes.button}
             />
-        </div>
+        )
 
-        <NextButton classes={classes} />
-    </div>
-)
+        const emailPage = (
+            <form onSubmit={this.handleSubmit} action='#'>
+                <div className={classes.center}>
+                    <TextField
+                        label='Sähköpostiosoite'
+                        type='email'
+                        name='email'
+                        required
+                        fullWidth
+                        autoFocus
+                        onChange={this.setFieldValue}
+                    />
+                </div>
 
-const PasswordPage = ({classes}) => (
-    <div>
-        <div className={classes.center}>
-            <TextField
-                label='Salasana'
-                type='password'
-                fullWidth
-            />
-        </div>
+                {nextButton}
+            </form>
+        )
 
-        <NextButton classes={classes} />
-    </div>
-)
+        const passwordPage = (
+            <form onSubmit={this.handleSubmit} action='#'>
+                <div className={classes.center}>
+                    <TextField
+                        label='Salasana'
+                        type='password'
+                        name='password'
+                        required
+                        fullWidth
+                        autoFocus
+                        onChange={this.setFieldValue}
+                    />
+                </div>
 
-const handleSubmit = (e, {page}) => {
-    e.preventDefault()
-    alert("submit")
+                {nextButton}
+            </form>
+        )
+        
+        const formContents = (
+            <React.Fragment>
+                <Typography variant='display1'>Kirjaudu sisään</Typography>
+                <Typography>käyttäen puoluetoimiston sinulle jakamia tunnuksia</Typography>
+
+                <SwipeableViews disabled index={this.state.page}>
+                    {emailPage}
+                    {passwordPage}
+                </SwipeableViews>
+            </React.Fragment>
+        )
+
+        return (
+            <Paper classes={{root: classes.root}}>
+                {formContents}
+            </Paper>
+        )
+
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+
+        if (this.state.page === PAGE_EMAIL) this.goPasswordPage()
+        if (this.state.page === PAGE_PASSWORD) this.initLogin()
+    }
+
+    setFieldValue = e => {
+        this.setState({
+            [e.target.name]     : e.target.value,
+        })
+    }
+
+    async initLogin () {
+
+        const loginResp = await fetch('/api/login', {
+            method              : 'POST',
+            headers             : {
+                'Content-Type'      : 'application/json',
+            },
+            body                : JSON.stringify({
+                email               : this.state.email,
+                password            : this.state.password,
+            })
+        }).then(resp => resp.json())
+
+        await localForage.setItem('auth_token', loginResp.token)
+
+    }
+
+    goEmailPage() {
+        this.setState({
+            page                : PAGE_EMAIL,
+        })
+    }
+
+    goPasswordPage() {
+        this.setState({
+            page                : PAGE_PASSWORD,
+        })
+    }
 }
-
-const LoginView = ({page, classes, onChangePage}) => (
-    <form onSubmit={e => handleSubmit(e, {page})}>
-        <Paper classes={{root: classes.root}}>
-
-            <Typography variant='display1'>Kirjaudu sisään</Typography>
-            <Typography>käyttäen puoluetoimiston sinulle jakamia tunnuksia</Typography>
-            
-            <SwipeableViews disabled>
-                <EmailPage classes={classes} />
-                <PasswordPage classes={classes} />
-            </SwipeableViews>
-
-        </Paper>
-    </form>
-)
 
 export default withStyles(styles)(LoginView)
