1382b94e9237509a7048ad76bf5b8ce72912fa0c
Petja <hello@petja.me>
2018-04-29 22:07:49 +0300

    Update constituencies database seed

--- seeds/02_constituencies.js
+++ seeds/02_constituencies.js
@@ -1,26 +1,34 @@
 exports.seed = async (knex, Promise) => {
-    // Deletes ALL existing entries
-    await knex('constituencies').del()
-
-    // Inserts seed entries
-    await knex('constituencies').insert([
+    const constituencies  = [
         // Ankkalinnan pormestarivaalit
-        {id: 1      , name: 'Itä-Ankkalinna'         , election: 1       , slug: 'east-duckburg'},
-        {id: 2      , name: 'Länsi-Ankkalinna'       , election: 1       , slug: 'west-duckburg'},
+        // id   name                                slug                election
+        [1,     'Itä-Ankkalinnan vaalipiiri',       'east-duckburg',    1],
+        [2,     'Länsi-Ankkalinnan vaalipiiri',     'west-duckburg',    1],
 
         // Eduskuntavaalit
-        {id: 3      , name: 'Helsinki'               , election: 2       , slug: 'helsinki'},
-        {id: 4      , name: 'Uusimaa'                , election: 2       , slug: 'uusimaa'},
-        {id: 5      , name: 'Varsinais-Suomi'        , election: 2       , slug: 'varsinais-suomi'},
-        {id: 6      , name: 'Satakunta'              , election: 2       , slug: 'satakunta'},
-        {id: 7      , name: 'Ahvenanmaa'             , election: 2       , slug: 'ahvenanmaa'},
-        {id: 8      , name: 'Häme'                   , election: 2       , slug: 'hame'},
-        {id: 9      , name: 'Pirkanmaa'              , election: 2       , slug: 'pirkanmaa'},
-        {id: 10     , name: 'Kaakkois-Suomi'         , election: 2       , slug: 'kaakkois-suomi'},
-        {id: 11     , name: 'Savo-Karjala'           , election: 2       , slug: 'savo-karjala'},
-        {id: 12     , name: 'Vaasa'                  , election: 2       , slug: 'vaasa'},
-        {id: 13     , name: 'Keski-Suomi'            , election: 2       , slug: 'keski-suomi'},
-        {id: 14     , name: 'Oulu'                   , election: 2       , slug: 'oulu'},
-        {id: 15     , name: 'Lappi'                  , election: 2       , slug: 'lappi'},
-    ])
+        // id   name                                slug                election
+        [3,     'Helsingin vaalipiiri',             'helsinki',         2],
+        [4,     'Uudenmaan vaalipiiri',             'uusimaa',          2],
+        [5,     'Varsinais-Suomen vaalipiiri',      'varsinais-suomi',  2],
+        [6,     'Satakunnan vaalipiiri',            'satakunta',        2],
+        [7,     'Ahvenanmaan vaalipiiri',           'ahvenanmaa',       2],
+        [8,     'Hämeen vaalipiiri',                'hame',             2],
+        [9,     'Pirkanmaan vaalipiiri',            'pirkanmaa',        2],
+        [10,    'Kaakkois-Suomen vaalipiiri',       'kaakkois-suomi',   2],
+        [11,    'Savo-Karjalan vaalipiiri',         'savo-karjala',     2],
+        [12,    'Vaasan vaalipiiri',                'vaasa',            2],
+        [13,    'Keski-Suomen vaalipiiri',          'keski-suomi',      2],
+        [14,    'Oulun vaalipiiri',                 'oulu',             2],
+        [15,    'Lapin vaalipiiri',                 'lappi',            2],
+    ]
+
+    // Insert seed entries
+    await knex('constituencies').insert(
+        constituencies.map(constituency => ({
+            id                      : constituency[0],
+            name                    : constituency[1],
+            slug                    : constituency[2],
+            election                : constituency[3],
+        }))
+    )
 }
